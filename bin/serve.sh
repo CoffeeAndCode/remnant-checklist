57edbbba1ba0c3f93c9b139969274e6461ce6a8e
Jonathan Knapp <jon@coffeeandcode.com>
2020-03-23 08:35:17 -0400

    Move things around; run web server from public/

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-wasm-pack build --target web
+wasm-pack build --out-dir public/pkg --target web
 
+cd public
 echo "Server started at http://localhost:8000 from $PWD"
 docker run -p 8000:80 --rm -v "$HOME/.profile_sources/server/custom-types.conf":/etc/nginx/conf.d/custom-types.conf:ro -v "$PWD":/usr/share/nginx/html:ro nginx:alpine
