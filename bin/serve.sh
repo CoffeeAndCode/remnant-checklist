51aaf402e5882296c445cabd4cfb1f2781d3e8a5
Jonathan Knapp <jon@coffeeandcode.com>
2020-03-23 08:32:41 -0400

    Add index.html so project can actually be ran

--- bin/serve.sh
+++ bin/serve.sh
@@ -2,6 +2,5 @@
 
 wasm-pack build --target web
 
-cd pkg
 echo "Server started at http://localhost:8000 from $PWD"
 docker run -p 8000:80 --rm -v "$HOME/.profile_sources/server/custom-types.conf":/etc/nginx/conf.d/custom-types.conf:ro -v "$PWD":/usr/share/nginx/html:ro nginx:alpine
