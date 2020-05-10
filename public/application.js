065ae59746b993f20277174cf9a35d8a8da1ff05
Jonathan Knapp <jon@coffeeandcode.com>
2020-05-10 00:41:54 -0400

    Minimal service worker to appease Chrome install

--- public/application.js
+++ public/application.js
@@ -1,5 +1,13 @@
 import init, { start } from "./pkg/remnant.js";
 
-init("./pkg/remnant_bg.wasm").then(_ => {
+init("./pkg/remnant_bg.wasm").then((_) => {
   start();
 }, console.error);
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register("/serviceworker.js", { scope: "/" })
+    .then(function () {
+      console.log("[Companion]", "Service worker registered!");
+    }, console.error);
+}
---
+++ public/serviceworker.js
@@ -0,0 +1,76 @@
+var CACHE_VERSION = "1";
+var CACHE_NAME = CACHE_VERSION + ":sw-cache-";
+
+var cachedURLs = [
+  "/application.js",
+  "/pkg/remnant.js",
+  "/pkg/remnant_bg.wasm",
+  "/index.html",
+  "https://cdn.jsdelivr.net/npm/todomvc-app-css@2.1.2/index.css",
+  "https://cdn.jsdelivr.net/npm/todomvc-common@1.0.5/base.css",
+];
+
+function onInstall(event) {
+  console.log("[Serviceworker]", "Installing!", event);
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function prefill(cache) {
+      return cache.addAll(cachedURLs);
+    })
+  );
+}
+
+function onActivate(event) {
+  console.log("[Serviceworker]", "Activating!", CACHE_NAME, event);
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+            return cacheName.indexOf(CACHE_VERSION) !== 0;
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+}
+
+function isHTMLRequest(request) {
+  return (
+    request.mode === "navigate" ||
+    (request.method === "GET" &&
+      request.headers.get("accept").includes("text/html"))
+  );
+}
+
+// Borrowed from https://github.com/TalAter/UpUp
+function onFetch(event) {
+  if (cachedURLs.includes(event.request.url) || isHTMLRequest(event.request)) {
+    event.respondWith(
+      // try to return untouched request from network first
+      fetch(event.request).catch(function () {
+        // if it fails, try to return request from the cache
+        return caches.match(event.request).then(function (response) {
+          if (response) {
+            return response;
+          }
+
+          // if not found in cache, return default offline content for navigate requests
+          if (isHTMLRequest(event.request)) {
+            console.log("[Serviceworker]", "Fetching offline content", event);
+            return caches.match("/offline.html");
+          }
+        });
+      })
+    );
+  }
+}
+
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+self.addEventListener("fetch", onFetch);
